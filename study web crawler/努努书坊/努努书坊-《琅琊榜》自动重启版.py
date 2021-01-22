a88d70b157f60ea797e511e24c98ef32102413df
penny-glo <75248193+penny-glo@users.noreply.github.com>
2021-01-22 14:49:24 +0800

    Update 努努书坊-《琅琊榜》自动重启版.py

--- study web crawler/努努书坊/努努书坊-《琅琊榜》自动重启版.py
+++ study web crawler/努努书坊/努努书坊-《琅琊榜》自动重启版.py
@@ -1,3 +1,12 @@
+'''
+1.24 写出可以自动重启的爬虫
+算是第一个自己完整实现所有预设功能的爬虫，里面还有两个小bug，但是不影响大致功能
+果然我对于白嫖的热爱才是学习的原动力啊
+大概花了一个晚上和两个上午时间写的爬虫，最开始是为了爬《甄嬛传》，但是发现没有自动重启功能的爬虫太肝了，搞得我差点迟到，才又琢磨了一早上写出了能自动重启的爬虫，并且拿《琅琊榜》debug
+还是值得纪念一下的~感觉算是对爬虫入门了，在微信里看到有学员交了几千块钱学了python爬虫做的项目，我这么一看我觉得我也能爬（捂脸）
+不过session还是个坑啊，鬼知道有生之年能不能成功爬进华电的教务系统……
+'''
+
 import requests
 import bs4
 from bs4 import BeautifulSoup
@@ -5,7 +14,7 @@ import re
 import time
 
 
-def getUrl(url):    #获取需爬取的网页地址(OK)
+def getUrl(url):    #获取需爬取的网页地址
     
     try:
         r=requests.get(url)
@@ -65,9 +74,8 @@ def getHTML(page):  #爬取网页
         return n
 
 
-def message(html):  #解析网页，获取所有小说内容(OK)
+def message(html):  #解析网页，获取所有小说内容
     soup=BeautifulSoup(html,'html.parser')
-
     all_p=soup.find_all('p')
     s = ''
     for string in all_p[1:-2]:
@@ -76,7 +84,6 @@ def message(html):  #解析网页，获取所有小说内容(OK)
     while True:                      #用换行符替换所有的'<br/>'
         index_begin = s.find("<")
         index_end = s.find(">",index_begin + 1)
-        
         if index_begin == -1:
             break
         s = s.replace(s[index_begin:index_end+1],"\n")
@@ -87,7 +94,7 @@ def message(html):  #解析网页，获取所有小说内容(OK)
 
 
 def txt(parseWeb):  #保存为txt文件
-    with open('C:\\Users\\mac\\Desktop\\琅琊榜\\琅琊榜1.txt','a',encoding='utf-8') as f:
+    with open('C:\\Users\\mac\\Desktop\\琅琊榜\\琅琊榜.txt','a',encoding='utf-8') as f:
         f.write(parseWeb + '\n')
 
 
